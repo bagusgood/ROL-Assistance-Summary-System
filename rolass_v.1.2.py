a943e15c14e122fa591af33fbb6d61b3479408c2
bagusgood <47693308+bagusgood@users.noreply.github.com>
2026-01-12 10:37:44 +0800

    Update rolass_v.1.2.py

--- rolass_v.1.2.py
+++ rolass_v.1.2.py
@@ -2253,6 +2253,7 @@ def get_cat(spt, kab, kec):
 
 if __name__ == "__main__":
     #app.run(debug=True, host="0.0.0.0", port=80)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
+
 
 
